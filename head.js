376babbb99c5055291379c6533f3e38b97a0412b
TejHunjan <gurtejh@gmail.com>
2021-06-22 19:47:01 +0000

    adding implement head function

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍🏼 Assertion Passed: ${actual} === ${expected}`);
+
+  } else if (actual !== expected) {
+    console.log(`👎🏼 😥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+head = function(array){
+ 
+ return array[0];
+};
+
+// head([5,6,7])
+
+assertEqual(head([5,6,7]), 5); 
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5); 
