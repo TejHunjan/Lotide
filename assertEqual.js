8eb828c04bbff91c72041cb1e6cc84c607fff34d
TejHunjan <gurtejh@gmail.com>
2021-06-22 17:45:50 +0000

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👍🏼 Assertion Passed: " + actual + " === " + expected);
+
+  } else if (actual !== expected) {
+    console.log("👎🏼 😥 Assertion Failed: " + actual + " != " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('minus 5', -5)
+assertEqual('happy', 'Happy')
+assertEqual(9, 3)
+assertEqual()
